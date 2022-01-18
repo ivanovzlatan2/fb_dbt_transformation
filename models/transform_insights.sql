8d5b16c8b9c29ef399f6a99d95bccd84841a8159
ivanovzlatan2 <72340466+ivanovzlatan2@users.noreply.github.com>
2022-01-18 20:18:21 +0200

    Update transform_insights.sql

--- models/transform_insights.sql
+++ models/transform_insights.sql
@@ -12,7 +12,7 @@ base as (
        nullif(campaign_id,'') as campaign_id,
        nullif(ad_id,'') as ad_id,
        nullif(adset_id,'') as adset_id,
-			 _time_loaded,
+			 _airbyte_emitted_at,
        ad_name,
        adset_name,
        campaign_name,
@@ -36,7 +36,7 @@ base as (
    cross join unnest(insights.actions)
 ),
 
-basic_report_actions as (select date, ad_id,_time_loaded,campaign_id, adset_id, ad_name, adset_name,account_name, campaign_name, cpc, cpm, ctr, frequency, impressions,inline_link_clicks,reach,objective,spend,
+basic_report_actions as (select date, ad_id,_airbyte_emitted_at,campaign_id, adset_id, ad_name, adset_name,account_name, campaign_name, cpc, cpm, ctr, frequency, impressions,inline_link_clicks,reach,objective,spend,
    any_value(if(action_type = "view_content", value, null)) as view_content,
    any_value(if(action_type = "omni_add_to_cart", value, null)) as add_to_cart,
    any_value(if(action_type = "omni_initiated_checkout", value, null)) as initiated_checkout,
@@ -53,7 +53,7 @@ basic_report_actions as (select date, ad_id,_time_loaded,campaign_id, adset_id, 
    any_value(if(action_type = "initiate_checkout", value, null)) as initiate_checkout,
    any_value(if(action_type = "complete_registration", value, null)) as complete_registration,
 from base
-group by base.ad_id, base.campaign_id,base._time_loaded,base.adset_id,base.date, ad_name, adset_name,account_name,campaign_name, cpc, cpm, ctr, frequency, impressions,inline_link_clicks,reach,objective,spend
+group by base.ad_id, base.campaign_id,base._airbyte_emitted_at,base.adset_id,base.date, ad_name, adset_name,account_name,campaign_name, cpc, cpm, ctr, frequency, impressions,inline_link_clicks,reach,objective,spend
 order by base.date ASC),
 
 action_values as (
@@ -90,7 +90,7 @@ group by action_values.ad_id,action_values.date
 ),
 
 basic_report_final as (
-select basic_report_actions.date as date, basic_report_actions.ad_id, _time_loaded, campaign_name,adset_name, ad_name,account_name,  adset_id, campaign_id, cpc, cpm, ctr, frequency, impressions, inline_link_clicks, reach,objective, spend, view_content, add_to_cart,add_to_cart_value,purchase,purchase_1dc,purchase_7dc,purchase_28dc,purchase_value_1dc,purchase_value_7dc,purchase_value_28dc,landing_page_view, purchase_value, comment,lead, messages, messages_7d, initiate_checkout, complete_registration from basic_report_actions
+select basic_report_actions.date as date, basic_report_actions.ad_id, _airbyte_emitted_at, campaign_name,adset_name, ad_name,account_name,  adset_id, campaign_id, cpc, cpm, ctr, frequency, impressions, inline_link_clicks, reach,objective, spend, view_content, add_to_cart,add_to_cart_value,purchase,purchase_1dc,purchase_7dc,purchase_28dc,purchase_value_1dc,purchase_value_7dc,purchase_value_28dc,landing_page_view, purchase_value, comment,lead, messages, messages_7d, initiate_checkout, complete_registration from basic_report_actions
 left join action_values
 ON basic_report_actions.date=action_values.date AND basic_report_actions.ad_id=action_values.ad_id
 ),
@@ -109,7 +109,7 @@ select * from (
 ),
 
 merge_campaign_names as (
-select basic_report_final.date as date, basic_report_final.ad_id, _time_loaded, basic_report_final.campaign_name,basic_report_final.adset_name, ad_name,basic_report_final.account_name,  adset_id, basic_report_final.campaign_id, cpc, cpm, ctr, frequency, impressions, inline_link_clicks, reach,objective, spend, view_content, add_to_cart,add_to_cart_value,purchase,purchase_1dc,purchase_7dc,purchase_28dc,purchase_value_1dc,purchase_value_7dc,purchase_value_28dc,landing_page_view, purchase_value, comment,lead, messages, messages_7d, initiate_checkout, complete_registration, current_campaign_name.campaign_name as current_campaign_name from basic_report_final
+select basic_report_final.date as date, basic_report_final.ad_id, _airbyte_emitted_at, basic_report_final.campaign_name,basic_report_final.adset_name, ad_name,basic_report_final.account_name,  adset_id, basic_report_final.campaign_id, cpc, cpm, ctr, frequency, impressions, inline_link_clicks, reach,objective, spend, view_content, add_to_cart,add_to_cart_value,purchase,purchase_1dc,purchase_7dc,purchase_28dc,purchase_value_1dc,purchase_value_7dc,purchase_value_28dc,landing_page_view, purchase_value, comment,lead, messages, messages_7d, initiate_checkout, complete_registration, current_campaign_name.campaign_name as current_campaign_name from basic_report_final
 left join current_campaign_name
 ON basic_report_final.campaign_id=current_campaign_name.campaign_id
 ),
@@ -128,7 +128,7 @@ select * from (
 ),
 
 merge_adset_names as (
-select merge_campaign_names.date as date, merge_campaign_names.ad_id, _time_loaded, merge_campaign_names.campaign_name,merge_campaign_names.adset_name, ad_name,merge_campaign_names.account_name,  merge_campaign_names.adset_id, merge_campaign_names.campaign_id, cpc, cpm, ctr, frequency, impressions, inline_link_clicks, reach,objective, spend, view_content, add_to_cart,add_to_cart_value,purchase,purchase_1dc,purchase_7dc,purchase_28dc,purchase_value_1dc,purchase_value_7dc,purchase_value_28dc,landing_page_view, purchase_value, comment,lead, messages, messages_7d, initiate_checkout, complete_registration,current_campaign_name,current_adset_name.adset_name as current_adset_name from merge_campaign_names
+select merge_campaign_names.date as date, merge_campaign_names.ad_id,_airbyte_emitted_at, merge_campaign_names.campaign_name,merge_campaign_names.adset_name, ad_name,merge_campaign_names.account_name,  merge_campaign_names.adset_id, merge_campaign_names.campaign_id, cpc, cpm, ctr, frequency, impressions, inline_link_clicks, reach,objective, spend, view_content, add_to_cart,add_to_cart_value,purchase,purchase_1dc,purchase_7dc,purchase_28dc,purchase_value_1dc,purchase_value_7dc,purchase_value_28dc,landing_page_view, purchase_value, comment,lead, messages, messages_7d, initiate_checkout, complete_registration,current_campaign_name,current_adset_name.adset_name as current_adset_name from merge_campaign_names
 left join current_adset_name
 ON merge_campaign_names.adset_id=current_adset_name.adset_id
 ),
@@ -147,7 +147,7 @@ select * from (
 ),
 
 merge_ad_names as (
-select merge_adset_names.date as date, merge_adset_names.ad_id, _time_loaded, merge_adset_names.campaign_name,merge_adset_names.adset_name, merge_adset_names.ad_name,merge_adset_names.account_name,  adset_id, merge_adset_names.campaign_id, cpc, cpm, ctr, frequency, impressions, inline_link_clicks, reach,objective, spend, view_content, add_to_cart,add_to_cart_value,purchase,purchase_1dc,purchase_7dc,purchase_28dc,purchase_value_1dc,purchase_value_7dc,purchase_value_28dc,landing_page_view, purchase_value, comment,lead, messages, messages_7d, initiate_checkout, complete_registration,current_campaign_name,merge_adset_names.current_adset_name, current_ad_name.ad_name as current_ad_name from merge_adset_names
+select merge_adset_names.date as date, merge_adset_names.ad_id, _airbyte_emitted_at, merge_adset_names.campaign_name,merge_adset_names.adset_name, merge_adset_names.ad_name,merge_adset_names.account_name,  adset_id, merge_adset_names.campaign_id, cpc, cpm, ctr, frequency, impressions, inline_link_clicks, reach,objective, spend, view_content, add_to_cart,add_to_cart_value,purchase,purchase_1dc,purchase_7dc,purchase_28dc,purchase_value_1dc,purchase_value_7dc,purchase_value_28dc,landing_page_view, purchase_value, comment,lead, messages, messages_7d, initiate_checkout, complete_registration,current_campaign_name,merge_adset_names.current_adset_name, current_ad_name.ad_name as current_ad_name from merge_adset_names
 left join current_ad_name
 ON merge_adset_names.ad_id=current_ad_name.ad_id
 )
@@ -189,7 +189,7 @@ select * from (
     current_adset_name,
     current_ad_name,
     
-    row_number() over(partition by ad_id, date order by _time_loaded desc) as rn
+    row_number() over(partition by ad_id, date order by _airbyte_emitted_at desc) as rn
     from merge_ad_names
     ) t
     where t.rn = 1
