2bd4fb817d8457c36ce7762097aa42065e5f7538
ivanovzlatan2 <72340466+ivanovzlatan2@users.noreply.github.com>
2022-02-04 21:06:42 +0200

    Update transform_insights.sql

--- models/transform_insights.sql
+++ models/transform_insights.sql
@@ -5,6 +5,10 @@ with insights as (
   select * from  development_test.fb_test_3_ads_insights
 ),
 
+
+-- Actions are already normalized from Airbyte in ..._ads_insights_actions. 
+-- This query extracts the necessary "action_types" from ..._ads_insights_actions by extracting the necessary "action_types", then sets them as columns and assign the corresponding value as the metric.
+
 actions as (
   select 
      _airbyte_fb_test_3_ads_insights_hashid as actions_id,
@@ -19,6 +23,9 @@ actions as (
   group by actions_id
 ),
 
+-- Action_Values are already normalized from Airbyte in ..._ads_insights_action_values. 
+-- This query extracts the necessary "action_types" from ..._ads_insights_action_values, then sets them as columns and assigns the corresponding value as the metric.
+
 action_values as (
   select 
      _airbyte_fb_test_3_ads_insights_hashid as action_values_id,
@@ -30,6 +37,9 @@ action_values as (
   group by action_values_id
 ),
 
+
+-- This query joins insights together with actions and action values on their "hashid". Only selected and necessary columns are specified.
+
 basic_report as (
   select 
     date_start, ad_id, _airbyte_emitted_at, campaign_name,adset_name, ad_name,account_name,  adset_id, campaign_id, cpc, cpm, ctr, frequency, impressions, inline_link_clicks, reach,objective, spend, view_content, add_to_cart,add_to_cart_value,purchase,purchase_value,landing_page_view, comment, initiate_checkout, complete_registration, 
@@ -39,6 +49,9 @@ basic_report as (
   left join  action_values
   ON insights._airbyte_fb_test_3_ads_insights_hashid=action_values.action_values_id
 ),
+
+-- Campaign_name is extracted from FB API and is subject to change on a daily basis, because it serves advertisers an annotation purpose.
+-- This query's result is producing a list of most recent campaign_names, partitioned by campaign_id.
 
 current_campaign_name as (
 select * from (
@@ -53,11 +66,17 @@ select * from (
     where t.rn = 1
 ),
 
+-- This query joins all previous data together with "current_campaign_name" as an additional column. 
+-- The "campaign_name" data is staying the same as when extracted.
+
 merge_campaign_names as (
 select basic_report.date_start as date, basic_report.ad_id, _airbyte_emitted_at, basic_report.campaign_name,basic_report.adset_name, ad_name,basic_report.account_name,  adset_id, basic_report.campaign_id, cpc, cpm, ctr, frequency, impressions, inline_link_clicks, reach,objective, spend, view_content, add_to_cart,add_to_cart_value,purchase,landing_page_view, purchase_value, comment,initiate_checkout, complete_registration, current_campaign_name.campaign_name as current_campaign_name from basic_report
 left join current_campaign_name
 ON basic_report.campaign_id=current_campaign_name.campaign_id
 ),
+
+-- Adset_name is extracted from FB API and is subject to change on a daily basis, because it serves advertisers an annotation purpose.
+-- This query's result is producing a list of most recent adset_names, partitioned by adset_id.
 
 current_adset_name as (
 select * from (
@@ -72,12 +91,17 @@ select * from (
     where t.rn = 1
 ),
 
+-- This query joins all previous data together with "current_adset_name" as an additional column. 
+-- The "adset_name" data is staying the same as when extracted.
 
 merge_adset_names as (
 select merge_campaign_names.date as date, merge_campaign_names.ad_id,_airbyte_emitted_at, merge_campaign_names.campaign_name,merge_campaign_names.adset_name, ad_name,merge_campaign_names.account_name,  merge_campaign_names.adset_id, merge_campaign_names.campaign_id, cpc, cpm, ctr, frequency, impressions, inline_link_clicks, reach,objective, spend, view_content, add_to_cart,add_to_cart_value,purchase,landing_page_view, purchase_value, comment, initiate_checkout, complete_registration,current_campaign_name,current_adset_name.adset_name as current_adset_name from merge_campaign_names
 left join current_adset_name
 ON merge_campaign_names.adset_id=current_adset_name.adset_id
 ),
+
+-- Ad_name is extracted from FB API and is subject to change on a daily basis, because it serves advertisers an annotation purpose.
+-- This query's result is producing a list of most recent ad_names, partitioned by ad_id.
 
 current_ad_name as (
 select * from (
@@ -92,11 +116,16 @@ select * from (
     where t.rn = 1
 ),
 
+-- This query joins all previous data together with "current_ad_name" as an additional column. 
+-- The "ad_name" data is staying the same as when extracted.
+
 merge_ad_names as (
 select merge_adset_names.date as date, merge_adset_names.ad_id, _airbyte_emitted_at, merge_adset_names.campaign_name,merge_adset_names.adset_name, merge_adset_names.ad_name,merge_adset_names.account_name,  adset_id, merge_adset_names.campaign_id, cpc, cpm, ctr, frequency, impressions, inline_link_clicks, reach,objective, spend, view_content, add_to_cart,add_to_cart_value,purchase,landing_page_view, purchase_value, comment, initiate_checkout, complete_registration,current_campaign_name,merge_adset_names.current_adset_name, current_ad_name.ad_name as current_ad_name from merge_adset_names
 left join current_ad_name
 ON merge_adset_names.ad_id=current_ad_name.ad_id
 )
+
+-- This query produces the table with the name "transform_insights" with all the necessary columns and data, current names and rows are filtered with impressions > 0 OR purchases > 0, because otherwise it produces empty rows of campaigns that have zero metric values.
 
 
 select * except(rn) from (
@@ -134,7 +163,4 @@ select * except(rn) from (
     row_number() over(partition by ad_id, date order by _airbyte_emitted_at desc) as rn
     from merge_ad_names
     ) t
-    where t.rn = 1 AND spend != 0 OR purchase != 0 
-
-
--- select * from basic_report
+    where t.rn = 1 AND spend != 0 OR purchase != 0
